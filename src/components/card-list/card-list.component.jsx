ccae43450fbbec61a622082dd8ed122a6a162b92
karannnaidu <karan.cs18@bmsce.ac.in>
2020-08-19 21:33:21 +0530

    adding files to git hub

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Card } from '../card/card.component'
+import './card-list.styles.css'
+export const CardList = props => (
+    //props creates the object of any properties used with CardList
+
+    <div className='card-list'>
+        {props.monsters.map(monster => (
+            <Card key={monster.id} monster={monster} />
+        ))}
+    </div>
+);
